cd2ca6d6cd02f1a437af81c2324e8624406ef6ac
barada-sahoo-git <barada.tech@gmail.com>
2023-03-04 17:43:31 +0530

    1st commit to dev branch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world,This is Barada Sahoo");
+
+        System.out.println("this is my new code");
     }
 }
